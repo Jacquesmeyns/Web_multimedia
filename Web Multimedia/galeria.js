29eb7fc9652a68ea0bc564e27569205f350bbc75
Jacques Meyns <jack_aguila55@hotmail.com>
2019-04-25 19:43:25 +0200

    Redistribución archivos de imagen
    archivos de imagen repartidos en carpetas

--- Web Multimedia/galeria.js
+++ Web Multimedia/galeria.js
@@ -6,7 +6,7 @@ function adelante()
 	if(num > 3)
 		num = 1;
 	var foto=document.getElementById("foto");
-	foto.src="./imagenes/foto"+num+".jpg";
+	foto.src="./imagenes/misc/foto"+num+".jpg";
 }
 function atras()
 {
@@ -14,5 +14,5 @@ function atras()
 	if(num < 1)
 		num = 3;
 	var foto=document.getElementById("foto");
-	foto.src="./imagenes/foto"+num+".jpg";
+	foto.src="./imagenes/misc/foto"+num+".jpg";
 }
